c5d93e0c2758259a1d60f1f053fad057580ae2c8
Alexey Krasnov <fort.kto2@gmail.com>
2018-10-07 14:22:43 +0400

    collaboration test

--- src/main/java/com/solidshake/JSnake/JSEngine.java
+++ src/main/java/com/solidshake/JSnake/JSEngine.java
@@ -5,15 +5,24 @@ public class JSEngine
     JSEngine() {
     	
     }
-    
-    public static void start() {
-    	Snake snake = new Snake();
-    	GameField field = new GameField();
-    	
-    	field.showSnake(snake);
-    	
-//    	while(true) {
-//    		
-//    	}
+
+    public static void onCreate() {
+
+        Snake snake = new Snake();
+        GameField field = new GameField();
+
+        field.showSnake(snake);
+    }
+
+    public static void onStart() {
+
+    }
+
+    public static void onUpdate() {
+
+    }
+
+    public static void gameOver() {
+
     }
 }
